be3a0488c86159c1953684a89acbf449d37c3b61
Dave Goehrig <dave@dloh.org>
2016-04-07 11:55:22 -0400

    merging diff branch

--- git.erl
+++ git.erl
@@ -3,13 +3,15 @@
 	init/0, 
 	commit/1, 
 	add/1,
+	add_all/0,
 	push/0, pull/0, pull/1, 
 	delete/1,
 	checkout/1,
 	feature/1,	
 	merge/1,
 	status/0, log/0,
-	branch/0
+	branch/0,
+	diff/1
 	]).
 
 
@@ -21,6 +23,9 @@ commit(Message) ->
 
 add(File)  ->
 	io:format("~s", [ os:cmd("git add " ++ File) ]).
+
+add_all()  ->
+	io:format("~s", [ os:cmd("git add *") ]).
 
 push() ->
 	io:format("~s", [ os:cmd("git push")] ).
@@ -40,6 +45,9 @@ feature(Branch) ->
 delete(Branch) ->
 	io:format("~s", [ os:cmd("git branch -D " ++ Branch)]).
 
+merge(Branch) ->
+	io:format("~s", [ os:cmd("git merge --squash " ++ Branch)]).
+
 status() ->
 	io:format("~s", [ os:cmd("git status") ]).
 	
@@ -48,3 +56,6 @@ log() ->
 
 branch() ->
 	io:format("~s", [ os:cmd("git branch") ]).
+
+diff(Tag) ->
+	io:format("~s", [ os:cmd("git diff " ++ Tag) ]).
