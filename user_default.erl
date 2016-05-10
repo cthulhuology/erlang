2f0832995508c8d4d282a762b8888f8c277c0525
Dave Goehrig <dave@dloh.org>
2016-05-10 09:53:40 -0400

    user path mods

--- user_default.erl
+++ user_default.erl
@@ -1,14 +1,77 @@
 -module(user_default).
--export([ vi/1 , home/0]).
+-export([ vi/1 , home/0, lm/0,
+	git_clone/1,
+	git_init/0, 
+	git_commit/1, 
+	git_add/1,
+	git_add_all/0,
+	git_push/0, git_pull/0, git_pull/1, 
+	git_delete/1,
+	git_checkout/1,
+	git_feature/1,	
+	git_merge/1,
+	git_status/0, git_log/0,
+	git_branch/0,
+	git_diff/1
+	]).
 
 vi(X) when is_atom(X) ->
-	File = atom_to_list(X),
-	case filelib:is_file(File ++ ".erl") of
-		true -> vi(File ++ ".erl");
-		false -> vi(File)
-	end;
+	vi(atom_to_list(X) ++ ".erl");
 vi(X) when is_list(X) ->
-	spawn(fun () -> os:cmd("gvim " ++ X ) end).
+	spawn(os,cmd, [ "gvim " ++ X ]).
 
 home() ->
-	c:cd("c:/Users/dave/Erlang").
+	c:cd(os:getenv("USERPROFILE") ++ "/Erlang").
+
+
+git_clone(Url) ->
+	io:format("~s", [ os:cmd("git clone " ++ Url) ]).
+
+git_init() ->
+	io:format("~s" , [ os:cmd("git init .") ]).
+
+git_commit(Message) ->
+	io:format("~s" , [ os:cmd("git commit -a -m \"" ++ Message ++ "\"") ]).
+
+git_add(File)  ->
+	io:format("~s", [ os:cmd("git add " ++ File) ]).
+
+git_add_all()  ->
+	io:format("~s", [ os:cmd("git add *") ]).
+
+git_push() ->
+	io:format("~s", [ os:cmd("git push")] ).
+
+git_pull() ->
+	io:format("~s", [ os:cmd("git pull")]).
+
+git_pull(Origin) ->
+	io:format("~s", [ os:cmd("git pull " ++ Origin)]).
+
+git_checkout(Branch) ->
+	io:format("~s", [ os:cmd("git checkout " ++ Branch)]).
+
+git_feature(Branch) ->
+	io:format("~s", [ os:cmd("git checkout -b " ++ Branch)]).
+
+git_delete(Branch) ->
+	io:format("~s", [ os:cmd("git branch -D " ++ Branch)]).
+
+git_merge(Branch) ->
+	io:format("~s", [ os:cmd("git merge --squash " ++ Branch)]).
+
+git_status() ->
+	io:format("~s", [ os:cmd("git status") ]).
+	
+git_log() ->
+	io:format("~s", [ os:cmd("git log") ]).
+
+git_branch() ->
+	io:format("~s", [ os:cmd("git branch") ]).
+
+git_diff(Tag) ->
+	io:format("~s", [ os:cmd("git diff " ++ Tag) ]).
+
+lm() ->
+	{ ok, Files } = file:list_dir(os:getenv("USERPROFILE") ++ "\\Erlang\\" ),
+	[ code:add_pathz(os:getenv("USERPROFILE") ++ "\\Erlang\\" ++ X ++ "\\ebin" ) || X <- lists:filtermap( fun(Y) -> filelib:is_dir(os:getenv("USERPROFILE") ++ "\\Erlang\\" ++ Y ++ "\\ebin") end, Files) ].
